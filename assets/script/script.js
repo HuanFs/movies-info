7c6fceeee568ffdc6f3ed8f5ab02cb78d2e6801e
Ruan Figuereido <huanfiguereido@gmail.com>
2023-09-29 16:54:32 -0300

    possivel resolução do erro mixed-contens que indica um erro de segurança durante a requisição http

--- assets/script/script.js
+++ assets/script/script.js
@@ -14,22 +14,26 @@ btn_reset.addEventListener("click", Clear);
 //função que processa e exibe os eresultados
 async function SearchMovie(){
     let title_of_movie = movie_title.value;
-    let url = await fetch(`http://www.omdbapi.com/?apikey=cdba40d8&t=${title_of_movie}`);
-    let response = await url.json();
-    console.log(response);
-    title.value=response.Title;
-    setTimeout((x)=>{
-        response.Title=='N/A'?year.value="Não encontrado":year.value=response.Year;
-    },1000);
-    setTimeout((x)=>{
-        response.Actors=='N/A'?actors.value="Não encontrado":actors.value=response.Actors;
-    },2000);
-    setTimeout((x)=>{
-        response.Director=='N/A'?direction.value="Não encontrado":direction.value=response.Director;
-    },3000);
-    setTimeout((x)=>{
-        response.Plot=='N/A'?sinopse.innerTextL="Não encontrado":sinopse.innerText=response.Plot;
-    },4000);
+    try{
+        let url = await fetch(`https://www.omdbapi.com/?apikey=cdba40d8&t=${title_of_movie}`);
+        let response = await url.json();
+        title.value=response.Title;
+        setTimeout((x)=>{
+         response.Title=='N/A'?year.value="Não encontrado":year.value=response.Year;
+         },1000);
+        setTimeout((x)=>{
+            response.Actors=='N/A'?actors.value="Não encontrado":actors.value=response.Actors;
+        },2000);
+        setTimeout((x)=>{
+            response.Director=='N/A'?direction.value="Não encontrado":direction.value=response.Director;
+        },3000);
+        setTimeout((x)=>{
+            response.Plot=='N/A'?sinopse.innerTextL="Não encontrado":sinopse.innerText=response.Plot;
+        },4000);
+    }
+    catch{(error)=>{
+        alert("Aconteçeu um erro com a sua solicitação, por favor, tente mais tarde")
+    }}
 }
 //função que limpa os campos
 function Clear(){
